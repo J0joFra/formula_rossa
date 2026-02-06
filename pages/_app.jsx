df243491d38de013ac75ebe177abb5e7b0f621d6
Joaquim Francalanci <94995228+J0joFra@users.noreply.github.com>
2026-02-06 15:15:54 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZCZQFLK4L"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-8ZCZQFLK4L');
+        </script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8762257220044998"
         crossorigin="anonymous"></script>
         {/* Tailwind CSS da CDN */}
